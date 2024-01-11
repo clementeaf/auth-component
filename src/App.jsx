6244aeae29e46929f516bf279d127c13b4c7a395
clementeaf <ext_carriagada@sbpay.cl>
2024-01-11 14:22:13 -0300

    AuthComponent init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AuthComponent from "./AuthComponent";
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-[#d5d5d5]/60">
+      <AuthComponent />
+    </div>
+  );
+}
+
+export default App;
