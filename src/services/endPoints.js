706c2727ec42c51333724126096ed4f5a3a7313a
clementeaf <ext_carriagada@sbpay.cl>
2024-01-12 23:37:45 -0300

    success config of endpoint to login

--- src/services/endPoints.js
+++ src/services/endPoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4500/";
+const BASE_URL = "https://user-microservice-production.up.railway.app/";
 
 export const REGISTER_API = BASE_URL + 'api/auth/registerUser';
 export const LOGIN_API = BASE_URL + 'api/auth/login';
